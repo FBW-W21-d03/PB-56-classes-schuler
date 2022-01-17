c4efec55f1ea9c6672fceabd1c0ee77051b96cb5
Mago <89070052+ShiinyFlakes@users.noreply.github.com>
2022-01-17 11:29:54 +0100

    Fix

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ this.nachname = nachname;
 this.klasse = klasse; 
 this.note = note; };
 }; 
-let schueler1 = new Classes_schueler("Nataly Miller", "10A", 2.8);
-let schuler2 = new Classes_schueler("Penny Blaese", "9B", 3.1);
-let schuler3 = new Classes_schueler("Jimmy Moore", "12D", 1.4); 
+let schueler1 = new schueler("Nataly Miller", "10A", 2.8);
+let schuler2 = new schueler("Penny Blaese", "9B", 3.1);
+let schuler3 = new schueler("Jimmy Moore", "12D", 1.4); 
 const namen = [schueler1, schueler2, schueler]; 
-console.log(namen)
+console.log(namen);
