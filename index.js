5805c30650b256bc551c6a57719a8179ece76aa7
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-17 10:53:54 +0100

    mein lösung

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let schuler_01 = new Classes_schuler("Nataly Miller", "10A", 2.8);
 let schuler_02 = new Classes_schuler("Penny Blaese", "9B", 3.1);
 let schuler_03 = new Classes_schuler("Jimmy Moore", "12D", 1.4);
 
+const namen = [schuler_01, schuler_02, schuler_03];
 
-console.log(schuler_01);
-console.log(schuler_02);
-console.log(schuler_03);
+console.log(namen)
