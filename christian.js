f56c908a3e096e19c556b1dd05d9b0a756281da0
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-17 11:10:25 +0100

    jetzt mit push-Alternative

--- christian.js
+++ christian.js
@@ -1,3 +1,5 @@
+// Teil 1
+
 class Schüler {
   constructor(name, klasse, note) {
     this.name = name;
@@ -6,10 +8,22 @@ class Schüler {
   }
 }
 
+// Teil 2
+
 let nataly = new Schüler("Nataly Miller", "10A", 2.8);
 let penny = new Schüler("Penny Blaese", "9B", 3.1);
 let jimmy = new Schüler("Jimmy Moore", "12D", 1.4);
 
-console.log(nataly);
-console.log(penny);
-console.log(jimmy);
+// Teil 3
+
+let gruppe = [nataly, penny, jimmy];
+
+console.log(gruppe);
+
+// Teil 3 - Alternative mit push()
+
+let array = []; // Erstellt ein leeres Array
+
+array.push(nataly, penny, jimmy); // pusht in das leere Array
+
+console.log(array); // Ausgabe
