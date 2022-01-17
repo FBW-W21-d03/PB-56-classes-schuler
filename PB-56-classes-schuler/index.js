189ba74737ea0fa271c08a2bf67c496008e001c9
Jan Czok <jan.czok@outlook.de>
2022-01-17 15:43:18 +0100

    Update index.js

--- PB-56-classes-schuler/index.js
+++ PB-56-classes-schuler/index.js
@@ -1,9 +1,21 @@
+function printGrades(){
+    let chosen = document.getElementById("schueler_auswahl").value;
+    schueler.forEach((person) =>{
+        if(person.vorName == chosen){
+            person.printGradesToConsole();
+        }
+    })
+}
+
 class Schueler {
-  constructor(vorName, klassenstufe, abschlussnote) {
-    this.vorName = vorName;
-    this.klassenstufe = klassenstufe;
-    this.abschlussnote = abschlussnote;
-  }
+    constructor(vorName, klassenstufe, abschlussnote) {
+        this.vorName = vorName;
+        this.klassenstufe = klassenstufe;
+        this.abschlussnote = abschlussnote;
+    }
+    printGradesToConsole(){
+        console.log("My final grade is:", this.abschlussnote);
+    }
 }
 
 let schueler = [];
@@ -14,10 +26,16 @@ let schueler_3 = new Schueler("Jimmy Moore", "12D ", 1.4);
 
 schueler.push(schueler_1, schueler_2, schueler_3);
 
-console.log(schueler);
 
-let eleSchule= schueler;
+let eleSchule = schueler;
 
-document.getElementById("shuler").innerHTML=eleSchule;
-
-
+/*
+Für jeden Schüler ein Listenelement
+*/
+schueler.forEach((person) => {
+    document.getElementById("shuler").innerHTML = 
+    // was schon drin steht
+    document.getElementById("shuler").innerHTML +
+    // neuer Schüler
+    "<li>" + person.vorName + " ist in der Klasse " + person.klassenstufe + "</li>"
+});
