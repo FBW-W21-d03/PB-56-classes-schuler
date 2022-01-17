a01ae98f6fc620bb5e4818c286d260bad3754477
sprotte <metatron1972@live.de>
2022-01-17 11:04:42 +0100

    sprotte

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ let klasse_A_01 = new Klasse("Nataly Miller", "10A", "2.8");
 let klasse_A_02 = new Klasse("Penny Blease", "9B", "3.1");
 let klasse_A_03 = new Klasse("Jimmy Moore", "12D", "1.4");
 
+const array = ["Nataly Miller", "Penny Blease", "Jimmy Moore"];
+console.log(array);
+
 console.log(klasse_A_01, klasse_A_02, klasse_A_03);
