a0a6f86e8a87d56883df72b799b5b5ccf79db134
Mago <89070052+ShiinyFlakes@users.noreply.github.com>
2022-01-17 11:26:09 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+class schueler 
+{ constructor (nachname, klassenstufe, abschlussnote){ 
+this.nachname = nachname; 
+this.klasse = klasse; 
+this.note = note; };
+}; 
+let schueler1 = new Classes_schueler("Nataly Miller", "10A", 2.8);
+let schuler2 = new Classes_schueler("Penny Blaese", "9B", 3.1);
+let schuler3 = new Classes_schueler("Jimmy Moore", "12D", 1.4); 
+const namen = [schueler1, schueler2, schueler]; 
+console.log(namen)
