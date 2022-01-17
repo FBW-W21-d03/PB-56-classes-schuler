7919a475be86cb0cc8873039f2dac652e9ed7407
kawan <89071329+kawanaza@users.noreply.github.com>
2022-01-17 15:04:08 +0100

    ich habe PB 56-classes geschickt!

---
+++ PB-56-classes-schuler/index.js
@@ -0,0 +1,23 @@
+class Schueler {
+  constructor(vorName, klassenstufe, abschlussnote) {
+    this.vorName = vorName;
+    this.klassenstufe = klassenstufe;
+    this.abschlussnote = abschlussnote;
+  }
+}
+
+let schueler = [];
+
+let schueler_1 = new Schueler("Nataly Miller", "10A ", 2.8);
+let schueler_2 = new Schueler("Penny Blaese", "9B", 3.1);
+let schueler_3 = new Schueler("Jimmy Moore", "12D ", 1.4);
+
+schueler.push(schueler_1, schueler_2, schueler_3);
+
+console.log(schueler);
+
+let eleSchule= schueler;
+
+document.getElementById("shuler").innerHTML=eleSchule;
+
+
